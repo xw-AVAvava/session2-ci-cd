5def7c0e00c2aee02b2d10dd376e7797069c2b05
xw-AVAvava <3414506335@qq.com>
2026-05-09 13:50:10 +0800

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,1 +1,15 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import joblib
 
+data = pd.read_csv('training_data.csv')
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
+
+model = LinearRegression()
+model.fit(X, y)
+
+joblib.dump(model, 'linear_model.pkl')
+
+with open('linear_model.txt', 'w') as f:
+    f.write(f'Coefficients: {model.coef_}\nIntercept: {model.intercept_}\n')
